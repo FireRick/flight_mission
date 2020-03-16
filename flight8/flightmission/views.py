036118b06d0dd659404395fca2f40f8af39a948d
firerick <rixd@qq.com>
2020-03-17 06:10:58 +0800

    Develop index page of flightmission-app.

--- flight8/flightmission/views.py
+++ flight8/flightmission/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.contrib.auth.decorators import login_required
+
+from flightmission.models import Mission
 
 
 # Create your views here.
+@login_required
 def index_view(request):
-    return render(request, 'flightmission/index.html')
+    missions = Mission.objects.all()
+    context = {
+        'missions': missions,
+    }
+    return render(request, 'flightmission/index.html', context=context)
 
